ba707486e15c9c4778d69bbac6a4bae6a943129d
DESKTOP-JS828RS\Admin <mdnguyen928@gmail.com>
2021-02-20 16:37:47 +0700

    Style and style Sheet

--- App.js
+++ App.js
@@ -6,71 +6,51 @@ import {
   View,
   Text,
   Button,
+  Touchable,
 } from 'react-native';
 
 const App = () => {
 
 // For view 1
 const [name, setName] = useState('Javascript')
-const [session, setSession] = useState({
-  number : 6,
-  title : 'state'
-})
-const [current, setCurrent] = useState(true)
-
-// For View 2
-const [number, setNumber] = useState(0)
-
 const onClickHandler = () => {
   setName('Programming with Kay')
-  setSession({
-    ...session,
-    title : 'new '
-  })
-  setCurrent(false)
-}
-
-const onClickChangeNumber = () => {
-  setNumber(number + 1)
 }
 
   return (
     <View style={styles.body}>
-      <View style={styles.body}>
         <Text style={styles.text}>{name}</Text>
-        <Text style={styles.text}>This is session number {session.number} and about {session.title}</Text>
-        <Text style={styles.text}>{current ? 'current session' : 'next session'}</Text>
-        <Button 
+        <View style={styles.button}>
+        <Button  
           title="Update state" 
           onPress={onClickHandler} 
         ></Button>
-      </View>
-
-      <View style={styles.body}>
-        <Text style={styles.text}>{number * 5}</Text>
-        <Button 
-          title="add"
-          onPress={onClickChangeNumber}
-        ></Button>
-        <Text style={styles.text}>You clicked {number} times</Text>
-      </View>
+        </View>
     </View>
   );
 };
 
 const styles = StyleSheet.create({
   body: {
-    flex: 1,
+    width: '100%',
+    height: '50%',
     alignItems: 'center',
     justifyContent: 'center',
-    backgroundColor: '#0000ff'
+    backgroundColor: '#0000ff',
+    borderWidth: 10,
+    borderColor: '#ff00ff',
+    borderRadius: 5
   },
   text: {
     color:'#ffffff',
-    fontSize: 20,
+    fontSize: 40,
     fontStyle: 'italic',
     margin: 10
-  }
+  },
+   button : {
+     width: 250,
+     height: 60
+   }
   
 });
 
