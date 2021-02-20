fbead378fb5d8b76ea27c0358f1a908977bd8af0
DESKTOP-JS828RS\Admin <mdnguyen928@gmail.com>
2021-02-20 22:29:19 +0700

    List scrollview and resfreshcontrol

--- App.js
+++ App.js
@@ -7,48 +7,59 @@ import {
   Text,
   Button,
   Touchable,
+  ScrollView,
+  RefreshControl,
 } from 'react-native';
 
 const App = () => {
 
-// For view 1
-const [name, setName] = useState('Javascript')
-const onClickHandler = () => {
-  setName('Programming with Kay')
-}
+  const [Items, setItems] = useState(
+    [ 
+      {key : 1, item: 'Item 1'}, 
+      {key : 2, item: 'Item 2'}, 
+      {key : 3, item: 'Item 3'}, 
+      {key : 4, item: 'Item 4'}, 
+      {key : 5, item: 'Item 5'}, 
+      {key : 6, item: 'Item 6'}, 
+      {key : 7, item: 'Item 7'}, 
+      {key : 8, item: 'Item 8'}, 
+      {key : 9, item: 'Item 9'}, 
+      {key : 10, item: 'Item 10'}, 
+      {key : 11, item: 'Item 11'}, 
+      {key : 12, item: 'Item 12'}, 
+    ]
+  )
+    
+    const [Refreshing, setRefreshing] = useState(false)
+    const onRefresh = () => {
+      setRefreshing(true);
+      setItems([...Items, {key: 60, item: 'Item 69'}])
+      setRefreshing(false)
+    }
 
   return (
+
     <View style={styles.body}>
-
-      <View style={styles.super1}>
-        <View style={styles.view1}>
-            <Text style={styles.text}>1</Text>
-          </View>
-          <View style={styles.view2}>
-            <Text style={styles.text}>2</Text>
-          </View>
-          <View style={styles.view3}>
-            <Text style={styles.text}>3</Text>
-          </View>
-      </View>
-
-      <View style={styles.super2}>
-        <View style={styles.view4}>
-          <Text style={styles.text}>4</Text>
-        </View>
-        <View style={styles.view5}>
-            <Text style={styles.text}>5</Text>
-          </View>
-      </View>
-
-      <View style={styles.super3}>
-        <View style={styles.view6}>
-          <Text style={styles.text}>6</Text>
-        </View>
-        <View style={styles.view7}>
-            <Text style={styles.text}>7</Text>
-        </View>
-      </View>
+      <ScrollView 
+        horizontal={false}
+        refreshControl={
+          <RefreshControl 
+            refreshing={Refreshing}
+            onRefresh={onRefresh}
+            colors={['blue']}
+          />
+        }
+      >
+        {Items.map((item) => {
+          return (
+            <View style={styles.item} key={item.key}>
+              <Text style={styles.text} >{item.item}</Text>
+            </View>
+          )
+        })}
+      </ScrollView>
+      
+      
     </View>
   );
 };
@@ -56,69 +67,18 @@ const onClickHandler = () => {
 const styles = StyleSheet.create({
   body: {
     flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
+    alignItems: 'stretch',
+    justifyContent: 'flex-start',
     backgroundColor: '#ffffff'
   },
-  super1: {
-    flex: 1,
-    flexDirection: 'row',
-    justifyContent: 'flex-start'
-  },
-  super2: {
-    flex: 2,
-    flexDirection: 'column',
-    alignSelf: 'stretch',
-  },
-  super3: {
-    flex: 6,
-    flexDirection: 'row',
-    justifyContent: 'center'
-  },
-  view1 : {
-    flex: 1,
-    justifyContent: 'center',
+  item : {
+    // flex: 1,
+    backgroundColor: '#ff00ff', 
     alignItems: 'center',
-    backgroundColor: '#00ffff'
-  },
-  view2 : {
-    flex: 2,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#ff00ff'
-  },
-  view3 : {
-    flex: 3,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#ffff00'
-  },
-  view4 : {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: 'red'
-  },
-  view5 : {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: 'green'
-  },
-  view6 : {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: 'white'
-  },
-  view7 : {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: 'blue'
+    margin: 10
   },
   text: {
-    fontSize: 20,
+    fontSize: 45,
     fontStyle: 'italic',
     margin: 10
   },
